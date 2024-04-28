133e77922b5f105febee4f0315477a2c47d9e316
Evgene Shlyk <serpupstars@gmail.com>
2024-04-28 10:43:00 +1000

    заканчивает последнее дз первого модуля

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,24 +1,52 @@
 import { render, RenderPosition } from '../render.js';
 import Filter from '../view/trip-filters.js';
 import TripSort from '../view/trip-sort.js';
+import TripPointsList from '../view/trip-points-list.js';
+import NewPoint from '../view/new-point.js';
+import EditPoint from '../view/edit-point.js';
+import Point from '../view/point.js';
 
 export default class Presenter {
   constructor() {
     this.tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
     this.mainPage = document.querySelector('.trip-events');
-
   }
 
-  renderFilter(){
+  renderFilter() {
     render(new Filter(), this.tripControlsFiltersElement);
   }
 
   renderTripSort() {
-    render(new TripSort(), this.mainPage)
+    render(new TripSort(), this.mainPage);
+  }
+
+  renderTripList() {
+    render(new TripPointsList(), this.mainPage);
+  }
+
+  renderEditPoint() {
+    let tripList = this.mainPage.querySelector('.trip-events__list');
+    render(new EditPoint(), tripList);
+  }
+
+  renderPoint() {
+    let tripList = this.mainPage.querySelector('.trip-events__list');
+    for (let i = 0; i < 3; i++) {
+      render(new Point(), tripList);
+    }
+  }
+
+  renderNewPoint() {
+    let tripList = this.mainPage.querySelector('.trip-events__list');
+    render(new NewPoint(), tripList);
   }
 
   init() {
     this.renderFilter();
     this.renderTripSort();
+    this.renderTripList();
+    this.renderEditPoint();
+    this.renderPoint();
+    this.renderNewPoint();
   }
 }
