5f3b7ed8b61c07334260426215cf891f4c3393c2
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2024-04-30 18:00:27 +1000

    30 вечер

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Presenter from './presenter/presenter.js';
 import TasksModel from './model/tasks-model.js';
 
+const filter = document.querySelector('.trip-controls__filters');
+const main = document.querySelector('.trip-events');
+
 const tasksModel = new TasksModel();
-const presenter = new Presenter({ tasksModel });
+const presenter = new Presenter({boardFilterContainer: filter, boardMainContainer: main, tasksModel });
 
 // const presenter = new Presenter();
 presenter.init();
+
+console.log(presenter);
--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -7,52 +7,57 @@ import EditPoint from '../view/edit-point.js';
 import Point from '../view/point.js';
 
 export default class Presenter {
-  constructor({tasksModel}) {
-    this.tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
-    this.mainPage = document.querySelector('.trip-events');
+  constructor({boardFilterContainer, boardMainContainer, tasksModel}) {
+    this.tripControlsFiltersElement = boardFilterContainer;
+    this.mainPage = boardMainContainer;
 
     this.tasksModel = tasksModel;
   }
 
-  renderFilter() {
+  // renderFilter() {
+  //   render(new Filter(), this.tripControlsFiltersElement);
+  // }
+  //
+  // renderTripSort() {
+  //   render(new TripSort(), this.mainPage);
+  // }
+  //
+  // renderTripList() {
+  //   render(new TripPointsList(), this.mainPage);
+  // }
+  //
+  // renderEditPoint() {
+  //   const tripList = this.mainPage.querySelector('.trip-events__list');
+  //   render(new EditPoint(), tripList);
+  // }
+  //
+  // renderPoint() {
+  //   const tripList = this.mainPage.querySelector('.trip-events__list');
+  //   for (let i = 0; i < 3; i++) {
+  //     render(new Point(), tripList);
+  //   }
+  // }
+  //
+  // renderNewPoint() {
+  //   const tripList = this.mainPage.querySelector('.trip-events__list');
+  //   render(new NewPoint(), tripList);
+  // }
+
+  init() {
+    this.boardTasks = [...this.tasksModel.tasks];
+
+
+
     render(new Filter(), this.tripControlsFiltersElement);
-  }
-
-  renderTripSort() {
     render(new TripSort(), this.mainPage);
-  }
-
-  renderTripList() {
     render(new TripPointsList(), this.mainPage);
-  }
-
-  renderEditPoint() {
     const tripList = this.mainPage.querySelector('.trip-events__list');
     render(new EditPoint(), tripList);
-  }
-
-  renderPoint() {
-    const tripList = this.mainPage.querySelector('.trip-events__list');
-    for (let i = 0; i < 3; i++) {
-      render(new Point(), tripList);
+    for (let i = 0; i < this.boardTasks.length; i++) {
+      render(new Point({task: this.boardTasks[i]}), tripList)
     }
-  }
 
-  renderNewPoint() {
-    const tripList = this.mainPage.querySelector('.trip-events__list');
     render(new NewPoint(), tripList);
   }
 
-  init() {
-    this.boardTasks = [...this.tasksModel.tasks]
-
-    this.renderFilter();
-    this.renderTripSort();
-    this.renderTripList();
-    this.renderEditPoint();
-    this.renderPoint();
-    this.renderNewPoint();
-
-
-  }
 }
