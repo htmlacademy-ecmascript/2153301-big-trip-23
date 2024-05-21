a1a230aa50061fe34ea031a174b9912b28eae32b
Evgene Shlyk <serpupstars@gmail.com>
2024-05-22 06:03:38 +1000

    исправляет ошибку

--- src/presenter/header-presenter.js
+++ src/presenter/header-presenter.js
@@ -20,7 +20,7 @@ export default class HeaderPresenter {
   }
 
   #renderFilters (filters) {
-    render(new TripFilterView(filters), this.#filterContainer);
+    render(new TripFilterView({filters}), this.#filterContainer);
   }
 
 }
