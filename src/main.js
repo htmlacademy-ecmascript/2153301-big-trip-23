1a79fc077917a681480c2e120f5e5267120a0db5
Evgene Shlyk <serpupstars@gmail.com>
2024-06-09 20:42:28 +1000

    7.7 заканчивает

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import MainPresenter from './presenter/main-presenter.js';
 import HeaderPresenter from './presenter/header-presenter';
 import PointModel from './model/point-model.js';
 import { generateFilter } from './mock/mock-filter.js';
+import FilterModel from './model/filter-model.js';
 
 const main = document.querySelector('.trip-events');
 const filterContainer = document.querySelector('.trip-controls__filters');
+const filterModel = new FilterModel();
 
 const pointModel = new PointModel();
 pointModel.init();
-
 
 const filters = generateFilter(pointModel.points);
 
