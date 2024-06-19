6c4c7a4d583470bf901f7c5de24078a18c5516f0
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2024-06-19 13:14:00 +1000

    удаляет ненужное

--- src/utils/grader.js
+++
@@ -1,8 +0,0 @@
-const generateSorterAndFilter = (grader, points) => Object.entries(grader)
-  .map(([sorterType, sorterPoints]) => ({
-    type: sorterType,
-    count: sorterPoints(points).length,
-  }),
-  );
-
-export {generateSorterAndFilter};
